ac733c5f94b3013c06a4665bd435b4e41b7e60bd
Andy <ajscottlewis@gmail.com>
2022-11-22 15:41:20 +0000

    Complete (except terminal output)

---
+++ databases_phase_two/book_store/lib/book.rb
@@ -0,0 +1,8 @@
+class Book
+  # Replace the attributes by your own columns.
+  attr_accessor :id, :title, :author_name
+end
+
+
+# The keyword attr_accessor is a special Ruby feature
+# which allows us to set and get attributes on an object
