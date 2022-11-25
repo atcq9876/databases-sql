4ccbc3ae2249d238beb82ee9f86db4b6d10fb83d
Andy <ajscottlewis@gmail.com>
2022-11-25 14:01:31 +0000

    Complete exercise: write app to print joined record to terminal

--- databases_phase_two/student_directory_2/app.rb
+++ databases_phase_two/student_directory_2/app.rb
@@ -3,3 +3,15 @@ require_relative 'lib/cohort'
 require_relative 'lib/student'
 require_relative 'lib/cohort_repository'
 
+DatabaseConnection.connect('student_directory_2')
+
+repo = CohortRepository.new
+cohort = repo.find_with_students(1)
+
+p "Cohort ID: #{cohort.id}"
+p "Cohort name: #{cohort.name}"
+p "Cohort starting date: #{cohort.starting_date}"
+p "Students in this cohort:"
+cohort.students.each do |record|
+  p "#{record.id} - #{record.name}"
+end
